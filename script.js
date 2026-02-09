852e66240e47a41c78047bf995bfcf104e49554b
Rahul Nokwal <nokwalrahul09@gmail.com>
2026-02-09 11:01:03 +0530

    add draggable feature and handels z-Indexing

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ appIcon.forEach((icon, index) => {
         ease: "back.out(1.7)",
       });
     }
+    icon.addEventListener("click", () => {
+      bringToFront(targetWindowOpen);
+    });
   });
   const title = document.querySelectorAll(".title");
   icon.addEventListener("mouseenter", () => {
@@ -37,6 +40,7 @@ appIcon.forEach((icon, index) => {
 });
 
 //closing the tabs
+const navbar = document.querySelector(".nav");
 const close = document.querySelectorAll(".close");
 close.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -47,37 +51,80 @@ close.forEach((btn) => {
       duration: 0.3,
       ease: "power5.out",
     });
+    gsap.to(navbar, {
+      delay: 2,
+    });
     navbar.classList.remove("nav-hide");
   });
 });
 
 // maximize the tab
+
 const maximize = document.querySelectorAll(".maximize");
 maximize.forEach((btn) => {
   btn.addEventListener("click", () => {
     var targetWindow = btn.closest(".desktop-window");
-    gsap.to(targetWindow, {
-      height: "100%",
-      width: "100%",
-      duration: 0.3,
-      ease: "power2.in",
-    });
-    navbar.classList.add("nav-hide");
+    if (targetWindow.style.height !== "100%") {
+      gsap.to(targetWindow, {
+        x: 0,
+        y: 0,
+        height: "100%",
+        width: "100%",
+        duration: 0.5,
+        ease: "power3.inOut",
+      });
+      navbar.classList.add("nav-hide");
+    } else {
+      gsap.to(targetWindow, {
+        x: 0,
+        y: 0,
+        height: "400px",
+        width: "600px",
+        duration: 0.5,
+        ease: "power4.inOut",
+      });
+      navbar.classList.remove("nav-hide");
+    }
   });
 });
 
 //minimize the tab
-const navbar = document.querySelector(".nav");
-const minimize = document.querySelectorAll(".minimize");
-minimize.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    var targetWindow = btn.closest(".desktop-window");
-    gsap.to(targetWindow, {
-      height: "400px",
-      width: "600px",
-      duration: 0.5,
-      ease: "power5.out",
-    });
-    navbar.classList.remove("nav-hide");
+
+// const minimize = document.querySelectorAll(".minimize");
+// minimize.forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     var targetWindow = btn.closest(".desktop-window");
+//     gsap.to(targetWindow, {
+//       height: "400px",
+//       width: "600px",
+//       duration: 0.5,
+//       ease: "power5.out",
+//     });
+//     navbar.classList.remove("nav-hide");
+//   });
+// });
+
+const desktop = document.querySelector(".desktop");
+const windows = document.querySelectorAll(".desktop-window");
+let zIdx = 100;
+
+function bringToFront(win) {
+  zIdx++;
+  win.style.zIndex = zIdx;
+}
+
+windows.forEach((win) => {
+  const windows_header = win.querySelector(".window-header");
+  Draggable.create(win, {
+    type: "x,y",
+    trigger: windows_header,
+    bounds: desktop,
+    edgeResistance: 0.65,
+    onPress: function () {
+      bringToFront(this.target);
+    },
+  });
+  win.addEventListener("click", () => {
+    bringToFront(win);
   });
 });
